69dee27426bad2ccf34219a83627d9c2237d8a6a
Kyle <kyle@kylemacquarrie.co.uk>
2019-03-03 21:30:55 +0000

    Rename and refactor exports slightly.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const dev = require('./webpack/dev.js')
-const production = require('./webpack/production.js')
-
 /**
  * Return the correct webpack config with customised options.
  * @param {string} title - Full sketch title. This will go in the <title> element.
@@ -9,10 +6,11 @@ const production = require('./webpack/production.js')
  * @param {string} [mode=development] - Webpack build mode, production or development
  * @returns {object} - Webpack config object
  */
+
 function config(title, slug, path, mode = 'development') {
-  return mode === 'production'
-    ? production(title, slug, path)
-    : dev(title, slug, path)
+  return mode === 'development'
+    ? require('./webpack/development.js')(title, slug, path)
+    : require('./webpack/production.js')(title, slug, path)
 }
 
 module.exports = {
